73a432725f04dbaa7f27837e132a1c2f36cbb178
Nathan V <nvahrenb@nd.edu>
2014-04-02 15:53:00 -0400

    Powerup on-screen

--- main.cpp
+++ main.cpp
@@ -22,6 +22,43 @@ using namespace std;
 
 int main(){
 
-	Board gameBoard;
+	// Test item display properties
+	
+	SDL_Surface* dotimg = NULL;
+	SDL_Surface* screen = NULL;
+	SDL_Event event;
+	
+	SDL_Init( SDL_INIT_EVERYTHING );
+	
+	screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+	
+	dotimg = SDL_LoadBMP( "dot.bmp" );
+	
+	int quit = 0;
+	
+	Item I1;
+	
+	while (quit == 0){
+	
+		while( SDL_PollEvent( &event ) ){
+			//handle_input();
+			if( event.type == SDL_QUIT ){
+				quit = 1;
+			}
+		}
+		
+		SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
+	
+		I1.display( dotimg, screen );
+	
+		SDL_Flip( screen );
+	
+		SDL_Delay( 100 );
+
+	}	
+
+	SDL_FreeSurface( dotimg );
+	
+	SDL_Quit();
 
 }
