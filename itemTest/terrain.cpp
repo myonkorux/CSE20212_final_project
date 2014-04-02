65d115c0874a9e827d203951b552f96193a66bde
cray1006 <cray1006@cray1006-UX31E.(none)>
2014-04-02 01:32:51 -0400

    creating testbench for item class

--- itemTest/terrain.cpp
+++ itemTest/terrain.cpp
@@ -6,11 +6,11 @@ Terrain::Terrain(int newX, int newY, int newType)
 	sety(newY);
 	setType(newType);
 
-	if(getType == 1)
+	if(getType() == 1)
 	{
 		TerrainImage = loadTerrainImage("rock2f.png");
 	}
-	else if(getType == 2)
+	else if(getType() == 2)
 	{
 		TerrainImage = loadTerrainImage("tree.png");
 	}
