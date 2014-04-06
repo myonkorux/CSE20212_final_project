500de0a58d33ebfb9423a425f60bca1fdabc2bdd
cray1006 <cray1006@cray1006-UX31E.(none)>
2014-04-06 15:25:02 -0400

    updated box

--- boxTest/box.h
+++ boxTest/box.h
@@ -6,24 +6,42 @@ Nathan Vahrenberg
 box.h
 
 *******************/
+#ifndef BOX_H
+#define BOX_H
 
 #include <iostream>
+#include<iomanip>
+#include<string>
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 
 using namespace std;
 
-class Box{
-
+class Box
+{
 	public:
 		Box();
-		void display();
+		void display(SDL_Surface *);
+		void setX(int);
+		void setY(int);
+		void setHeight(int);
+		void setWidth(int);
+		void setMessage(string);
+		int getX();
+		int getY();
+		int getHeight();
+		int getWidth();
+		void renderBox();
 		virtual void update() = 0;
 	
-	protected:
+	private:
 		int height;
 		int width;
 		int x;
 		int y;
+		string message;
 
+		SDL_Rect box;
 };
+
+#endif
