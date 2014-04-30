5a7ca0f5cce6c6dba4ee68b1006d35a266600bd7
cray1006 <cray@nd.edu>
2014-04-29 21:25:43 -0400

    text class now commented

--- text.h
+++ text.h
@@ -1,13 +1,21 @@
+//Christopher Ray, Nathan Vahrenberg
+//Professor Scott
+//CSE 20212
+//29 April 2014
+
+//text.h
+//Header file for Text class, for use with text.cpp in slayerz
+
 #ifndef TEXT_H
 #define TEXT_H
 
 #include"box.h"
 
-class Text : public Box
+class Text : public Box 				//text class inherits from box
 {
 	public:
-		Text(int, int, int, int, int, string);
-		virtual int update(SDL_Event );
+		Text(int, int, int, int, int, string); 	//constructor
+		virtual int update(SDL_Event ); 	//dummy update function (only here due to requirements of inheritance/polymorphism)
 };	
 
 #endif
